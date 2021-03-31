464b4a78d24234b21004be6067462e1ab5166859
Pierre Lardet <lardet.pierre@gmail.com>
2021-03-31 17:11:42 +0100

    feex

--- functions/create_account.php
+++ functions/create_account.php
@@ -31,30 +31,30 @@ if (mysqli_num_rows($result) != 0) {
     $_SESSION["createAccountError"] = true;
     unset($_SESSION['loggedIn']);
     header('Location: ../pages/sign_up.php');
+} else {
+    $_SESSION["createAccountError"] = false;
+    $sql = "INSERT INTO member (email, password, firstname, surname, avatar) 
+        VALUES ('$email', '$password', '$firstname', '$surname', '$avatar');";
+
+    $result = mysqli_query($conn, $sql);
+    if (!$result) {
+        die('Failed to execute query');
+    }
+
+    $sql = "SELECT ID FROM member WHERE email='$email'";
+    $result = mysqli_query($conn, $sql);
+
+    if (!$result || mysqli_num_rows($result) == 0) {
+        die('Error fetching database information');
+    }
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $id = $row["ID"];
+
+    $_SESSION["ID"] = $id;
+    $_SESSION["loggedIn"] = true;
+    $_SESSION["firstname"] = $firstname;
+    $_SESSION["surname"] = $surname;
+    $_SESSION["avatar"] = $avatar;
+    mysqli_close($conn);
+    header('Location: ../index.php');
 }
-
-$_SESSION["createAccountError"] = false;
-$sql = "INSERT INTO member (email, password, firstname, surname, avatar) 
-    VALUES ('$email', '$password', '$firstname', '$surname', '$avatar');";
-
-$result = mysqli_query($conn, $sql);
-if (!$result) {
-    die('Failed to execute query');
-}
-
-$sql = "SELECT ID FROM member WHERE email='$email'";
-$result = mysqli_query($conn, $sql);
-
-if (!$result || mysqli_num_rows($result) == 0) {
-    die('Error fetching database information');
-}
-$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-$id = $row["ID"];
-
-$_SESSION["ID"] = $id;
-$_SESSION["loggedIn"] = true;
-$_SESSION["firstname"] = $firstname;
-$_SESSION["surname"] = $surname;
-$_SESSION["avatar"] = $avatar;
-mysqli_close($conn);
-header('Location: ../index.php');
